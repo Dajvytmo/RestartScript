be97428acf8e3e7c727c6de250443e423e2696cf
Dávid Varga <92118691+Dajvytmo@users.noreply.github.com>
2021-12-03 10:43:54 +0100

    Pridany orig parameter s metodami

--- restart.py
+++ restart.py
@@ -7,6 +7,10 @@ arg_length = len(sys.argv)
 def full():
     print("full")
 
+# orig method
+def orig():
+    print("orig")
+
 # blue method
 def blue():
     print("blue")
@@ -14,6 +18,10 @@ def blue():
 # green method
 def green():
     print("green")
+
+# orig no restart method
+def origNR():
+    print("orig_no_restart")
 
 # blue no restart method
 def blueNR():
@@ -27,9 +35,13 @@ def greenNR():
 if arg_length == 1:
     full()
 
-# blue and green
+# orig, blue and green
 elif arg_length == 2 :
-    if sys.argv[1] == "blue":
+    
+    if sys.argv[1] == "orig":
+        orig()
+
+    elif sys.argv[1] == "blue":
         blue()
 
     elif sys.argv[1] == "green":
@@ -40,7 +52,14 @@ elif arg_length == 2 :
 
 # blue and green with no restart
 elif arg_length == 3:
-    if sys.argv[1] == "blue":
+
+    if sys.argv[1] == "orig":
+        if sys.argv[2] == "norestart":
+            origNR()
+        else:
+            print("Wrong argument")
+
+    elif sys.argv[1] == "blue":
         if sys.argv[2] == "norestart":
             blueNR()
         else:
