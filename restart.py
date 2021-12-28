c0f18fd9f416f7e72807250104fd736301858895
Dávid Varga <92118691+Dajvytmo@users.noreply.github.com>
2021-12-28 15:26:16 +0100

    Pridanie otacania instancii pre blue a green

--- restart.py
+++ restart.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, time
  
 # length of arguments
 arg_length = len(sys.argv)
@@ -58,10 +58,49 @@ copy_green_conf_287 = ["ssh 2a00:0da9:0004:202e:60fd:0000:2286:5287 'cd /home/ec
 "ssh 2a00:0da9:0004:202e:60fd:0000:2286:5287 'cd /home/ecare/apps/apache-prod-tibet-intern-proxy/conf/; cp http-instance.conf_green http-instance.conf'"]
 
 # graceful restart all proxy apaches for dcplnx22840332
-restart_apaches_332 = os.system("ssh 2a00:0da9:0004:202e:60fd:0000:2284:0332 'cd /home/ecare/scripts/; ./apaches_proxy.sh graceful'")
+restart_apaches_332 = "ssh 2a00:0da9:0004:202e:60fd:0000:2284:0332 'cd /home/ecare/scripts/; ./apaches_proxy.sh graceful'"
 
 # graceful restart all proxy apaches for dcplnx22865287
-restart_apaches_287 = os.system("ssh 2a00:0da9:0004:202e:60fd:0000:2286:5287 'cd /home/ecare/scripts/; ./apaches_proxy.sh graceful'")
+restart_apaches_287 = "ssh 2a00:0da9:0004:202e:60fd:0000:2286:5287 'cd /home/ecare/scripts/; ./apaches_proxy.sh graceful'"
+
+# delete text file with PIDs
+delete_pids_file = "rm -rf pids.txt"
+
+# find PID of all blue instances and store it to text file
+store_blue_pids = ["ssh ecare@qde5nj ps -ef | grep -i wildfly20_prod_11003 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qde5nj ps -ef | grep -i wildfly20_prod_11004 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2d ps -ef | grep -i wildfly20_prod_11001 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2d ps -ef | grep -i wildfly20_prod_11002 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2f ps -ef | grep -i wildfly20_prod_11002 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2f ps -ef | grep -i wildfly20_prod_11003 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2b ps -ef | grep -i wildfly20_prod_11003 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2b ps -ef | grep -i wildfly20_prod_11004 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2h ps -ef | grep -i wildfly20_prod_11003 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2h ps -ef | grep -i wildfly20_prod_11004 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2k ps -ef | grep -i wildfly20_prod_11003 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2k ps -ef | grep -i wildfly20_prod_11004 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt"]
+
+# find PID of all green instances and store it to text file
+store_green_pids = ["ssh ecare@qde5nj ps -ef | grep -i wildfly20_prod_11001 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qde5nj ps -ef | grep -i wildfly20_prod_11002 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2d ps -ef | grep -i wildfly20_prod_11003 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2d ps -ef | grep -i wildfly20_prod_11004 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2f ps -ef | grep -i wildfly20_prod_11001 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2f ps -ef | grep -i wildfly20_prod_11004 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2b ps -ef | grep -i wildfly20_prod_11001 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2b ps -ef | grep -i wildfly20_prod_11002 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2h ps -ef | grep -i wildfly20_prod_11001 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2h ps -ef | grep -i wildfly20_prod_11002 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2k ps -ef | grep -i wildfly20_prod_11001 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt",
+"ssh ecare@qdef2k ps -ef | grep -i wildfly20_prod_11002 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt"]
+
+# stop instances separately
+stop_instances = ["ssh ecare@qde5nj kill -9 ",
+"ssh ecare@qdef2d kill -9 ",
+"ssh ecare@qdef2f kill -9 ",
+"ssh ecare@qdef2b kill -9 ",
+"ssh ecare@qdef2h kill -9 ",
+"ssh ecare@qdef2k kill -9 "]
 
 # start blue instances separately 
 start_blue_instances = ["ssh ecare@qde5nj 'export RUN_CONF=/home/ecare/apps/wildfly20_prod_11003/configuration/standalone.conf && cd /home/ecare/apps/wildfly/wildfly-20.0.1.Final/bin/ && nohup ./standalone.sh -Djboss.server.base.dir=/home/ecare/apps/wildfly20_prod_11003/ -b=0.0.0.0 -bmanagement=0.0.0.0 >/dev/null 2>&1 &'",
@@ -91,22 +130,53 @@ start_green_instances = ["ssh ecare@qde5nj 'export RUN_CONF=/home/ecare/apps/wil
 "ssh ecare@qdef2k 'export RUN_CONF=/home/ecare/apps/wildfly20_prod_11001/configuration/standalone.conf && cd /home/ecare/apps/wildfly/wildfly-20.0.1.Final/bin/ && nohup ./standalone.sh -Djboss.server.base.dir=/home/ecare/apps/wildfly20_prod_11001/ -b=0.0.0.0 -bmanagement=0.0.0.0 >/dev/null 2>&1 &'",
 "ssh ecare@qdef2k 'export RUN_CONF=/home/ecare/apps/wildfly20_prod_11002/configuration/standalone.conf && cd /home/ecare/apps/wildfly/wildfly-20.0.1.Final/bin/ && nohup ./standalone.sh -Djboss.server.base.dir=/home/ecare/apps/wildfly20_prod_11002/ -b=0.0.0.0 -bmanagement=0.0.0.0 >/dev/null 2>&1 &'"]
 
+# open text file, read PIDs stored in file, execute kill commands and close text file - stop instances proccess
+def stop_instances_proc():
+    file = open("pids.txt", "r")
+    content = file.readlines() # read content of pids.txt file
+    line_num = -1
+
+    for cmnd in stop_instances:
+        line_num += 1
+        os.system(cmnd + content[line_num])
+        line_num += 1
+        os.system(cmnd + content[line_num])
+
+    file.close()
+
 # full restart method
 def full():
     print("full")
 
 # orig method
 def orig():
-    os.system("rm -rf pids.txt")
-    os.system("ssh ecare@qde5nj ps -ef | grep -i wildfly20_prod_11001 | grep -v grep | grep keystore | gawk '{print $2}' >> pids.txt")
+    print("orig")
 
 # blue method
 def blue():
+    blueNR()
+
+    os.system(delete_pids_file)
+    for cmnd in store_blue_pids:
+        os.system(cmnd)
+
+    stop_instances_proc()
+    time.sleep(5) # delay script for 5 seconds
+
     for cmnd in start_blue_instances:
         os.system(cmnd)
 
 # green method
 def green():
+    greenNR()
+
+    os.system(delete_pids_file)
+    for cmnd in store_green_pids:
+        os.system(cmnd)
+
+    stop_instances_proc()
+    time.sleep(5) # delay script for 5 seconds
+
     for cmnd in start_green_instances:
         os.system(cmnd)
 
@@ -114,28 +184,28 @@ def green():
 def origNR():
     for cmnd in copy_orig_conf_332:
         os.system(cmnd) 
-    restart_apaches_332
+    os.system(restart_apaches_332)
     for cmnd in copy_orig_conf_287:
         os.system(cmnd) 
-    restart_apaches_287
+    os.system(restart_apaches_287)
 
 # blue no restart method
 def blueNR():
     for cmnd in copy_blue_conf_332:
         os.system(cmnd) 
-    restart_apaches_332
+    os.system(restart_apaches_332)
     for cmnd in copy_blue_conf_287:
         os.system(cmnd) 
-    restart_apaches_287
+    os.system(restart_apaches_287)
 
 # green no restart method
 def greenNR():
     for cmnd in copy_green_conf_332:
         os.system(cmnd) 
-    restart_apaches_332
+    os.system(restart_apaches_332)
     for cmnd in copy_green_conf_287:
         os.system(cmnd) 
-    restart_apaches_287
+    os.system(restart_apaches_287)
 
 # full restart
 if arg_length == 1:
